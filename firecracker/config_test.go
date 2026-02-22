7d4c6c1f1786ff293196a2b1adc5d1cf59591563
Joakim Bakke Hellum <joakim.hellum@protonmail.ch>
2026-02-22 22:16:48 +0100

    adding config tests

---
+++ firecracker/config_test.go
@@ -0,0 +1,100 @@
+package firecracker
+
+import (
+	"testing"
+
+	"github.com/pigeon-as/nomad-driver-firecracker/firecracker/boot_source"
+	"github.com/pigeon-as/nomad-driver-firecracker/firecracker/drive"
+	"github.com/pigeon-as/nomad-driver-firecracker/firecracker/jailer"
+)
+
+func TestConfig_Validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		cfg     *Config
+		wantErr bool
+	}{
+		{"nil config", nil, false},
+		{"missing jailer", &Config{}, true},
+		{
+			"missing image_paths",
+			&Config{Jailer: &jailer.JailerConfig{ExecFile: "firecracker", JailerBinary: "jailer"}},
+			true,
+		},
+		{
+			"valid minimal",
+			&Config{
+				Jailer:     &jailer.JailerConfig{ExecFile: "firecracker", JailerBinary: "jailer"},
+				ImagePaths: []string{"/opt/images"},
+			},
+			false,
+		},
+		{
+			"relative image_path",
+			&Config{
+				Jailer:     &jailer.JailerConfig{ExecFile: "firecracker", JailerBinary: "jailer"},
+				ImagePaths: []string{"relative/path"},
+			},
+			true,
+		},
+		{
+			"empty image_path",
+			&Config{
+				Jailer:     &jailer.JailerConfig{ExecFile: "firecracker", JailerBinary: "jailer"},
+				ImagePaths: []string{""},
+			},
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.cfg.Validate()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Validate() err = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestTaskConfig_Validate(t *testing.T) {
+	validBoot := &boot_source.BootSource{KernelImagePath: "vmlinux"}
+	rootDrive := drive.Drive{PathOnHost: "/rootfs.ext4", IsRootDevice: true}
+	dataDrive := drive.Drive{PathOnHost: "/data.ext4"}
+
+	tests := []struct {
+		name    string
+		cfg     *TaskConfig
+		wantErr bool
+	}{
+		{"nil config", nil, false},
+		{"missing boot_source", &TaskConfig{Drives: []drive.Drive{rootDrive}}, true},
+		{"missing drives", &TaskConfig{BootSource: validBoot}, true},
+		{"no root device", &TaskConfig{BootSource: validBoot, Drives: []drive.Drive{dataDrive}}, true},
+		{
+			"two root devices",
+			&TaskConfig{
+				BootSource: validBoot,
+				Drives: []drive.Drive{
+					{PathOnHost: "/a.ext4", IsRootDevice: true},
+					{PathOnHost: "/b.ext4", IsRootDevice: true},
+				},
+			},
+			true,
+		},
+		{
+			"valid minimal",
+			&TaskConfig{BootSource: validBoot, Drives: []drive.Drive{rootDrive}},
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.cfg.Validate()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Validate() err = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
